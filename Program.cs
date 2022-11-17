14ce105fed9a883b056a8a0cc7218b5812b27a46
VoldMarius <ukusi@mail.ru>
2022-11-17 14:39:34 +0300

    Massiv

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿int[] myArray = { 822, 45, 632, 14, 25, 89 };
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write(myArray[i] + "  ");
+}
+Console.WriteLine();
+// Метод разворота массива
+void ReversalMassiv(int[] Arr)
+{
+    int length = Arr.Length;
+    if (Arr.Length % 2 == 0)
+    {
+        length = length / 2;
+    }
+    else
+        length = (length - 1) / 2;
+
+    for (int i = 0; i < length; i++)
+    {
+        int a = Arr[i];
+        Arr[i] = Arr[Arr.Length - 1 - i];
+        Arr[Arr.Length - 1 - i] = a;
+    }
+    for (int i = 0; i < Arr.Length; i++)
+    {
+        Console.Write(Arr[i] + "  ");
+    }
+}
+ReversalMassiv(myArray);
+
+Console.WriteLine();
+
+// Метод нахождения суммы (не)чётных элемента массива
+
+void EvenFind(int[] Array)
+{
+    int summa = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] % 2 == 0)
+        {
+            summa = summa + Array[i];
+        }
+    }
+    Console.WriteLine(summa);
+}
+EvenFind(myArray);
+
+// Наименьшее(наибольшее) в массиве
+
+void Less_moreMassiv(int[] Array)
+{
+    int max = 0;
+    for (int i = 1; i < Array.Length; i++)
+    {
+        if (Array[i] > Array[0])
+        {
+            max = Array[i];
+        }
+    }
+    Console.WriteLine(max);
+}
+Less_moreMassiv(myArray);
